334f59bab6a9bdf8d48165044fb1996f54ee3f19
imrezaulkrm <sayem010ahmed@gmail.com>
2025-01-03 01:47:29 +0600

    add new feature

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// New feature route
+app.get('/new', (req, res) => {
+    res.send('This is the new app feature!');
+});
+
+// Original route
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
