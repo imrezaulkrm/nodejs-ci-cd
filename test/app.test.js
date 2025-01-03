f8386fc74389d193008ca7f2285af9b6228432ac
imrezaulkrm <sayem010ahmed@gmail.com>
2025-01-03 17:26:35 +0600

    update test file

--- test/app.test.js
+++ test/app.test.js
@@ -1,21 +1,45 @@
-const assert = require('assert');
+const express = require('express');
 const request = require('supertest');
-const app = require('../app'); // Import your app
+const app = express();
 
-describe('GET /', function() {
-  it('should return "Hello, World!"', function(done) {
-    request(app)
-      .get('/')
-      .expect(200)
-      .expect('Hello, World!', done);
-  });
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
 });
 
-describe('GET /new', function() {
-  it('should return "This is the new app feature!"', function(done) {
-    request(app)
-      .get('/new')
-      .expect(200)
-      .expect('This is the new app feature!', done);
-  });
+app.get('/new', (req, res) => {
+    res.send('This is the new app feature!');
+});
+
+let server;
+
+before(() => {
+    // Start the server before running the tests
+    server = app.listen(3000, () => {
+        console.log('App listening at http://localhost:3000');
+    });
+});
+
+after(() => {
+    // Close the server after the tests are finished
+    server.close(() => {
+        console.log('Server shut down');
+    });
+});
+
+describe('GET /', function () {
+    it('should return "Hello, World!"', function (done) {
+        request(server)
+            .get('/')
+            .expect(200)
+            .expect('Hello, World!', done);
+    });
+});
+
+describe('GET /new', function () {
+    it('should return "This is the new app feature!"', function (done) {
+        request(server)
+            .get('/new')
+            .expect(200)
+            .expect('This is the new app feature!', done);
+    });
 });
